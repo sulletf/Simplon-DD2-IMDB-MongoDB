4d615889979344f9bbdb64f9b35c359f259772f4
Fred Sullet <fred@Fred>
2019-02-18 11:01:17 +0100

    prepare methods to fill

--- Scrapping.py
+++ Scrapping.py
@@ -9,6 +9,8 @@ class FilmList:
     self.year = year
 
   def download_html(self):
+    # telechargement des pages HTML 'coming soon' de tous les mois de l'annee
+
     for month in range(1,13):
       url = 'https://www.imdb.com/movies-coming-soon/{:04d}-{:02d}'.format(self.year,month)
       r = requests.get(url)
@@ -23,6 +25,8 @@ class FilmList:
       f.close()
 
   def get_films_urls(self):
+    # Recuperation des URLs des films de chaque mois
+
     films = []
 
     for month in range(1,13):
@@ -45,43 +49,25 @@ class Film:
     self.id = l[2]
 
   def download_html(self):
+    # telechargement de la page HTML du film
+
     r = requests.get(self.url)
 
     if r.status_code != 200: return
 
     soup = BeautifulSoup(r.content, 'html.parser')
 
-    title = None
-
-    for elem in soup.find_all('div',attrs={"class" :"title_wrapper"}):
-      title = elem.h1.text
-      break
-
     f = open('data/films/{}'.format(self.id), 'w')
     f.write(str(soup))
     f.close()
     
   def scrap(self):
-    f = open('data/films/{}'.format(self.id), 'r')
-    soup = BeautifulSoup(f, 'html.parser')
+    #  A faire : Scrapping de la page HTML
 
-    self.title = soup.find('h1').text
+  def load(self):
+    # A faire : enregistrement du film dans MongoDB
 
-    self.grossUSA = self.get_amount(soup, 'Gross USA')
-    self.grossWW = self.get_amount(soup, 'Cumulative Worldwide Gross')
+  def visu(self):
+    # A faire : creation des viz permettant de faire l'analyse des acteurs les 
+    # plus rentables
 
-  def get_amount(self, soup, amount_type):
-    amount = None
-    for div in soup.find_all('div',attrs={"class" :"txt-block"}):
-      h4 = div.find('h4',attrs={"class" :"inline"})
-      if h4 is not None and h4.text == amount_type + ':':
-        amount = div.text.split('\n')[1]
-        # amount is like 'Gross USA: $36,343,858, 15 March 2018'
-        # we clean it
-        pattern = '^' + amount_type + ': \$((\d+,?)+)(,.+?)?$'
-        x = re.match(pattern, amount)
-        amount = x.group(1)
-
-    return amount
-     
-#    self.actors = self.scrap_actors(soup)
